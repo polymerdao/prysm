e934b00a29946c47dfaf7954467209bfea1e95d3
Lizhang <lizhang@polymerlabs.org>
2023-02-22 00:17:43 +0800

    refactor: try to solve lint problem

--- beacon-chain/blockchain/process_block.go
+++ beacon-chain/blockchain/process_block.go
@@ -359,22 +359,7 @@ func (s *Service) onBlock(ctx context.Context, signed interfaces.SignedBeaconBlo
 	finalized := s.ForkChoicer().FinalizedCheckpoint()
 
 	// LightClientFinalityUpdate needs super majority
-	if finalized.Epoch > s.lastPublishedLightClientEpoch {
-		config := params.BeaconConfig()
-		if finalized.Epoch > config.AltairForkEpoch {
-			syncAggregate, err := signed.Block().Body().SyncAggregate()
-			if err == nil {
-				if syncAggregate.SyncCommitteeBits.Count()*3 >= config.SyncCommitteeSize*2 {
-					_, err := s.sendLightClientFinalityUpdate(ctx, signed, postState)
-					if err != nil {
-						log.WithError(err)
-					} else {
-						s.lastPublishedLightClientEpoch = finalized.Epoch
-					}
-				}
-			}
-		}
-	}
+	s.tryPublishLightClientFinalityUpdate(ctx, signed, finalized, postState)
 
 	if finalized.Epoch > currStoreFinalizedEpoch || (finalized.Epoch == postStateFinalizedEpoch && finalized.Epoch > preStateFinalizedEpoch) {
 		if err := s.updateFinalized(ctx, &ethpb.Checkpoint{Epoch: finalized.Epoch, Root: finalized.Root[:]}); err != nil {
@@ -416,6 +401,25 @@ func (s *Service) onBlock(ctx context.Context, signed interfaces.SignedBeaconBlo
 	}
 	onBlockProcessingTime.Observe(float64(time.Since(startTime).Milliseconds()))
 	return nil
+}
+
+func (s *Service) tryPublishLightClientFinalityUpdate(ctx context.Context, signed interfaces.SignedBeaconBlock, finalized *forkchoicetypes.Checkpoint, postState state.BeaconState) {
+	if finalized.Epoch > s.lastPublishedLightClientEpoch {
+		config := params.BeaconConfig()
+		if finalized.Epoch > config.AltairForkEpoch {
+			syncAggregate, err := signed.Block().Body().SyncAggregate()
+			if err == nil {
+				if syncAggregate.SyncCommitteeBits.Count()*3 >= config.SyncCommitteeSize*2 {
+					_, err := s.sendLightClientFinalityUpdate(ctx, signed, postState)
+					if err != nil {
+						log.WithError(err)
+					} else {
+						s.lastPublishedLightClientEpoch = finalized.Epoch
+					}
+				}
+			}
+		}
+	}
 }
 
 func getStateVersionAndPayload(st state.BeaconState) (int, interfaces.ExecutionData, error) {
