521fadc35b3f72e14e97911cc0fe47db107d2687
Giuseppe Valente <giuseppe@polymerlabs.org>
2023-02-23 17:03:50 -0800

    fix: goimpots

--- beacon-chain/rpc/prysm/v1alpha1/debug/p2p.go
+++ beacon-chain/rpc/prysm/v1alpha1/debug/p2p.go
@@ -2,11 +2,11 @@ package debug
 
 import (
 	"context"
-	"github.com/libp2p/go-libp2p/core/protocol"
 
 	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/libp2p/go-libp2p/core/network"
 	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/protocol"
 	"github.com/prysmaticlabs/prysm/v3/beacon-chain/p2p"
 	ethpb "github.com/prysmaticlabs/prysm/v3/proto/prysm/v1alpha1"
 	"google.golang.org/grpc/codes"
