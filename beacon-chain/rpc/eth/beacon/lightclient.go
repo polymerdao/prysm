22ab28644d31cad25d9915a60bdabed8b1cc9a36
Nicolás Pernas Maradei <nicolas@polymerlabs.org>
2023-08-31 14:43:06 +0000

    add GetLightClientUpdatesByRange rpc endpoint

--- beacon-chain/rpc/eth/beacon/lightclient.go
+++ beacon-chain/rpc/eth/beacon/lightclient.go
@@ -8,6 +8,11 @@ import (
 	"google.golang.org/grpc/status"
 
 	lightclienthelpers "github.com/prysmaticlabs/prysm/v4/beacon-chain/rpc/eth/helpers/lightclient"
+	"github.com/prysmaticlabs/prysm/v4/beacon-chain/state"
+	"github.com/prysmaticlabs/prysm/v4/config/params"
+	"github.com/prysmaticlabs/prysm/v4/consensus-types/interfaces"
+	types "github.com/prysmaticlabs/prysm/v4/consensus-types/primitives"
+	"github.com/prysmaticlabs/prysm/v4/encoding/bytesutil"
 	ethpbv2 "github.com/prysmaticlabs/prysm/v4/proto/eth/v2"
 )
 
@@ -46,3 +51,150 @@ func (bs *Server) GetLightClientBootstrap(ctx context.Context, req *ethpbv2.Ligh
 	return result, nil
 }
 
+// GetLightClientUpdatesByRange - implements https://github.com/ethereum/beacon-APIs/blob/263f4ed6c263c967f13279c7a9f5629b51c5fc55/apis/beacon/light_client/updates.yaml
+func (bs *Server) GetLightClientUpdatesByRange(ctx context.Context, req *ethpbv2.LightClientUpdatesByRangeRequest) (*ethpbv2.LightClientUpdatesByRangeResponse, error) {
+	// Prepare
+	ctx, span := trace.StartSpan(ctx, "beacon.GetLightClientUpdatesByRange")
+	defer span.End()
+
+	// Determine slots per period
+	config := params.BeaconConfig()
+	slotsPerPeriod := uint64(config.EpochsPerSyncCommitteePeriod) * uint64(config.SlotsPerEpoch)
+
+	// Adjust count based on configuration
+	count := uint64(req.Count)
+	if count > config.MaxRequestLightClientUpdates {
+		count = config.MaxRequestLightClientUpdates
+	}
+
+	// Determine the start and end periods
+	startPeriod := req.StartPeriod
+	endPeriod := startPeriod + count - 1
+
+	// The end of start period must be later than Altair fork epoch, otherwise, can not get the sync committee votes
+	startPeriodEndSlot := (startPeriod+1)*slotsPerPeriod - 1
+	if startPeriodEndSlot < uint64(config.AltairForkEpoch)*uint64(config.SlotsPerEpoch) {
+		startPeriod = uint64(config.AltairForkEpoch) * uint64(config.SlotsPerEpoch) / slotsPerPeriod
+	}
+
+	headState, err := bs.HeadFetcher.HeadState(ctx)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Could not get head state: %v", err)
+	}
+
+	lHeadSlot := uint64(headState.Slot())
+	headPeriod := lHeadSlot / slotsPerPeriod
+	if headPeriod < endPeriod {
+		endPeriod = headPeriod
+	}
+
+	// Populate updates
+	var updates []*ethpbv2.LightClientUpdateWithVersion
+	for period := startPeriod; period <= endPeriod; period++ {
+		// Get the last known state of the period,
+		//    1. We wish the block has a parent in the same period if possible
+		//	  2. We wish the block has a state in the same period
+		lLastSlotInPeriod := period*slotsPerPeriod + slotsPerPeriod - 1
+		if lLastSlotInPeriod > lHeadSlot {
+			lLastSlotInPeriod = lHeadSlot
+		}
+		lFirstSlotInPeriod := period * slotsPerPeriod
+
+		// Let's not use the first slot in the period, otherwise the attested header will be in previous period
+		lFirstSlotInPeriod++
+
+		var state state.BeaconState
+		var block interfaces.ReadOnlySignedBeaconBlock
+		for lSlot := lLastSlotInPeriod; lSlot >= lFirstSlotInPeriod; lSlot-- {
+			state, err = bs.Stater.StateBySlot(ctx, types.Slot(lSlot))
+			if err != nil {
+				continue
+			}
+
+			// Get the block
+			latestBlockHeader := *state.LatestBlockHeader()
+			latestStateRoot, err := state.HashTreeRoot(ctx)
+			if err != nil {
+				continue
+			}
+			latestBlockHeader.StateRoot = latestStateRoot[:]
+			blockRoot, err := latestBlockHeader.HashTreeRoot()
+			if err != nil {
+				continue
+			}
+
+			block, err = bs.BeaconDB.Block(ctx, blockRoot)
+			if err != nil || block == nil {
+				continue
+			}
+
+			syncAggregate, err := block.Block().Body().SyncAggregate()
+			if err != nil || syncAggregate == nil {
+				continue
+			}
+
+			if syncAggregate.SyncCommitteeBits.Count()*3 < config.SyncCommitteeSize*2 {
+				// Not enough votes
+				continue
+			}
+
+			break
+		}
+
+		if block == nil {
+			// No valid block found for the period
+			continue
+		}
+
+		// Get attested state
+		attestedRoot := block.Block().ParentRoot()
+		attestedBlock, err := bs.BeaconDB.Block(ctx, attestedRoot)
+		if err != nil || attestedBlock == nil {
+			continue
+		}
+
+		attestedSlot := attestedBlock.Block().Slot()
+		attestedState, err := bs.Stater.StateBySlot(ctx, attestedSlot)
+		if err != nil {
+			continue
+		}
+
+		// Get finalized block
+		var finalizedBlock interfaces.ReadOnlySignedBeaconBlock
+		finalizedCheckPoint := attestedState.FinalizedCheckpoint()
+		if finalizedCheckPoint != nil {
+			finalizedRoot := bytesutil.ToBytes32(finalizedCheckPoint.Root)
+			finalizedBlock, err = bs.BeaconDB.Block(ctx, finalizedRoot)
+			if err != nil {
+				finalizedBlock = nil
+			}
+		}
+
+		update, err := lightclienthelpers.NewLightClientUpdateFromBeaconState(
+			ctx,
+			config,
+			slotsPerPeriod,
+			state,
+			block,
+			attestedState,
+			finalizedBlock,
+		)
+
+		if err == nil {
+			updates = append(updates, &ethpbv2.LightClientUpdateWithVersion{
+				Version: ethpbv2.Version(attestedState.Version()),
+				Data:    update,
+			})
+		}
+	}
+
+	if len(updates) == 0 {
+		return nil, status.Errorf(codes.NotFound, "No updates found")
+	}
+
+	result := ethpbv2.LightClientUpdatesByRangeResponse{
+		Updates: updates,
+	}
+
+	return &result, nil
+}
